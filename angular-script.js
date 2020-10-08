1da76558f11dc847c645f1bbd9847f0235d6980d
Ginazai <rafaeldc1300@gmail.com>
2020-10-08 00:27:50 -0500

    commit

---
+++ angular-script.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  angular.module('food_list', [])
+  .controller('list_controller', list_controller);
+
+  list_controller.$inject = ['$scope'];
+  function list_controller($scope) {
+    $scope.input = "";
+    $scope.msg = "";
+
+    $scope.getValue = function() {
+      var m = getFood($scope.input);
+      $scope.msg = m;
+    };
+
+    function getFood(inpt) {
+      var reg = /\w+/;
+      var spc = /\s+/;
+      var pieces = inpt.split(",");
+      var msg= "";
+      for (var i = 0; i < pieces.length; i++) {
+        if (!pieces[i].match(reg)) {pieces.splice(i, 1);}
+      }
+      if (pieces.length <= 3) {msg = "Enjoy !";
+      $scope.myStyle ={'color': 'green'};}
+      else{if (pieces.length > 3) {msg = "Too much !";
+      $scope.myStyle = {'color': 'red'};}}
+      return msg;
+    }
+
+  }
+})();
